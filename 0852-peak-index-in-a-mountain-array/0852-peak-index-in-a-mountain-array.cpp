921a03a35325837e90cf63ab0bd8054eb0d277ae
chirag kumar chouhan <chiragneeraj2@gmail.com>
2026-07-06 17:08:57 +0530

    Time: 0 ms (100%), Space: 63.6 MB (10.81%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -4,18 +4,20 @@ public:
         int n = arr.size();
         int low = 0;
         int high = n - 1;
+        int ans;
         while (low < high) {
             int mid = low + (high - low) / 2;
             if (arr[mid] < arr[mid + 1]) {
                 // search on right
                 low = mid + 1;
             } else  {
+                ans=mid;
                 high = mid; // mid could be the high point
 
             } 
              
         }
         
-        return low;
+        return ans;
     }
 };
