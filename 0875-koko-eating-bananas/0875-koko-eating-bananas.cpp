2911df3254702b542ccc7ac116f19590fe444321
chirag kumar chouhan <chiragneeraj2@gmail.com>
2026-07-09 17:17:41 +0530

    Time: 8 ms (55.38%), Space: 23 MB (47.91%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    long long  fun(vector<int>& a,int speed) { // make a function that return in long long 
+    long long fun(vector<int>& a, int speed) {
         int n = a.size();
-       long long   h = 0;  // take it as long long 
+        long long h = 0;
         for (int i = 0; i < n; i++) {
             h = h + a[i] / speed;
-            if (a[i] % speed != 0) {
+            if(a[i]%speed!=0){ // mean h is not fully divided by speed mean it should increase by 1
                 h++;
             }
         }
         return h;
     }
-
     int minEatingSpeed(vector<int>& piles, int h) {
-        int n = piles.size();
-        int low = 1;
-        int high = *max_element(piles.begin(), piles.end());
-        long long result = -1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            long long  hours = fun(piles,mid); // mid in this mean speed in that function // take it as long long 
-            if (hours > h) {
-                low = mid + 1; // search in right
-            } else {
-                result = mid;
-                high = mid - 1;
+        int n=piles.size();
+        int low=1;
+        int result=-1;
+        int high= *max_element(piles.begin(), piles.end());
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            long long hourse=fun(piles,mid);
+            if(hourse>h){
+                //search in right
+                low=mid+1;
+            }else{
+                result=mid;
+                high=mid-1;
             }
         }
         return result;
